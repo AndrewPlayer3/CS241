c08fa7efe85e945476a2d8a98fe9be074b65c7d2
Andrew Player <andrewplayer3@gmail.com>
2022-02-20 19:52:49 -0900

    Adding Timing and Hertz Monitoring

--- Lab6 - 7x7 LED Matrix/LED_7x7/LED_7x7.ino
+++ Lab6 - 7x7 LED Matrix/LED_7x7/LED_7x7.ino
@@ -1,9 +1,9 @@
 /*/
- * Print Patterns on 7x7 LED Display
+ * Print Patterns on 8x8 LED Display
  * Andrew Player
  * 2022
  * 
- * Simple 7x7 LED Matrix Drawing program.
+ * Simple 8x8 LED Matrix Drawing program.
 /*/
 
 #include "LED_Display.hpp"
@@ -24,25 +24,57 @@ const uint8_t hard_wire_pins[n_hard_wire_pins] = {12, 11, 10, 9, 8, 7, 6, 5    }
 /*/
 const uint8_t pixel_drawing[n_pixels] = 
 {
-    0b11111111,
+    0b11100111,
     0b00000000,
     0b01100110,
     0b01100110,
     0b00000000,
-    0b11000011,
     0b01111110,
-    0b00000000
+    0b01000010,
+    0b00111100
 };
 
-LED_Display display(shift_pins, hard_wire_pins, pixel_drawing);
+LED_Display display;
 
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Serial Activated!");
+    display.init(shift_pins, hard_wire_pins, pixel_drawing);
 }
+
+int counter    = 0;
+int iterations = 1000;
+
+unsigned long draw_time  = 0;
 
 void loop() 
 {
+    unsigned long start_t = micros();
+
+    /* -------------------- */
     display.drawDisplay();
+    /* -------------------- */
+
+    unsigned long end_t   = micros();
+
+    draw_time += (end_t - start_t);
+
+    if (counter == iterations)
+    {
+      double avg_timing = (double)draw_time / (double)iterations;
+
+      double hertz      = (1000000 / avg_timing);
+
+      Serial.print  ("Average time to display: ");
+      Serial.print  (avg_timing                 );
+      Serial.print  (" microseconds, which is " );
+      Serial.print  (hertz                      );
+      Serial.println(" hertz."                  );
+
+      draw_time = 0;
+      counter   = 0;
+    }
+
+    else counter++;
 }
